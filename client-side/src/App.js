123c9e47718e87f69cec69ac5c2c3c717ab8e2ca
rotitrash <sanidhyasamaiya@gmail.com>
2023-04-05 11:45:27 +0530

    routes working

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AuthApi from './utils/AuthApi';
+import DashBoard from './components/DashBoard';
+// import Routes from './routes/Routes';
 
 // import Routes from './routes/Routes';
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter>
+     
         <Routes>
+          <Route path="/dashboard" element={<DashBoard/>}/>
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" Component={<SignUp />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
-      </BrowserRouter> */}
-      <SignIn/>
+
     </div>
   );
 }
