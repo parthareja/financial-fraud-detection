dc78027057224ad13418d7da4a32f70225028694
Parth Thareja <parth.thareja.pt@gmail.com>
2023-05-03 17:04:17 +0530

    model err fix 2

--- server-side/models/QueryData.js
+++ server-side/models/QueryData.js
@@ -10,38 +10,38 @@ const queryDataSchema = new mongoose.Schema({
         type: Int16Array,
         required: [true, "Please enter time step"],
       },
-      amount: {
-        type: Number,
-        required: [true, "Please enter amt"],
-      },
-      oldbalanceOrg: {
-        type: Number,
-        required: [true, "Please enter amt"],
-      },
-      oldbalanceDest: {
-        type: Number,
-        required: [true, "Please enter amt"],
-      },
-      origBalance_inacc: {
-        type: Number,
-        required: [true, "Please enter amt"],
-      },
-      destBalance_inacc: {
-        type: Number,
-        required: [true, "Please enter amt"],
-      },
-      type_CASH_OUT: {
-        type: Int16Array,
-        required: [true, "Please enter transaction type"],
-        min: [0, "bw 0 and 1"],
-        max: [1, "bw 0 and 1"],
-      },      
-      type_TRANSFER: {
-        type: Int16Array,
-        required: [true, "Please enter transaction type"],
-        min: [0, "bw 0 and 1"],
-        max: [1, "bw 0 and 1"]
-      },
+    amount: {
+      type: Number,
+      required: [true, "Please enter amt"],
+    },
+    oldbalanceOrg: {
+      type: Number,
+      required: [true, "Please enter amt"],
+    },
+    oldbalanceDest: {
+      type: Number,
+      required: [true, "Please enter amt"],
+    },
+    origBalance_inacc: {
+      type: Number,
+      required: [true, "Please enter amt"],
+    },
+    destBalance_inacc: {
+      type: Number,
+      required: [true, "Please enter amt"],
+    },
+    type_CASH_OUT: {
+      type: Int16Array,
+      required: [true, "Please enter transaction type"],
+      min: [0, "bw 0 and 1"],
+      max: [1, "bw 0 and 1"],
+    },      
+    type_TRANSFER: {
+      type: Int16Array,
+      required: [true, "Please enter transaction type"],
+      min: [0, "bw 0 and 1"],
+      max: [1, "bw 0 and 1"]
+    },
 });
 
 
