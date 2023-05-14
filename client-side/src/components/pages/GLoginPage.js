5676b34bfe6717d1e5ad2cf2687b658cc246b7fb
Aman Kaif <aman220202@gmail.com>
2023-05-14 13:12:47 +0530

    Login screen beautified

--- client-side/src/components/pages/GLoginPage.js
+++ client-side/src/components/pages/GLoginPage.js
@@ -3,17 +3,25 @@ import React from "react";
 import Login from "../GOAuth/Login";
 import Logout from "../GOAuth/Logout";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import Col from "react-bootstrap/Col";
+import { Container } from "@mui/material";
 
 const GLoginPage = () => {
   // console.log("glogin page");
   return (
-    <div className="flex">
-      <Col>Wow</Col>
-      <Col>
-        Half
-        <Login />
-      </Col>
+    <div style={{ height: "100vh" }} className="flex">
+      <div className="flex w-3/4">
+        <img style={{ objectFit: "cover" }} src="../../../img/greenLogin.jpg" />
+      </div>
+      <div
+        style={{ backgroundColor: "rgb(230, 235, 229)" }}
+        className="flex w-1/4 p-4"
+      >
+        <div style={{ position: "absolute", top: "50%" }}>
+          <Login />
+        </div>
+      </div>
 
       {/* <Logout /> */}
     </div>
